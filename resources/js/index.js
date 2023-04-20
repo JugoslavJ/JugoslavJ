564ff6a675faaec5f4e58d9f269145e7457740da
Jugoslav Jovicic <JugoslavJovicic@pm.me>
2023-04-20 20:45:09 +0200

    Nav links updated with styling and js

--- resources/js/index.js
+++ resources/js/index.js
@@ -25,10 +25,41 @@ async function showOnlyThisParagraph(paragraph){
     }
 }
 
+function clearSelectedClass(){
+    let navLinks = document.querySelectorAll(".links a");
+    navLinks.forEach( link => {
+        link.classList.remove("selected");
+    });
+}
+
+function addSelectedClass(link){
+    clearSelectedClass();
+    link.classList.add("selected");
+}
+
+function setSelectedByUrl(){
+    let element = '';
+    let targetIds = ['about-me', 'projects', 'skills'];
+    for(let i = 0; i < targetIds.length; i++){
+        if(window.location.href.includes("#" + targetIds[i])){
+            element = document.querySelectorAll("." + targetIds[i]);
+        }
+    }
+    if(element.length > 0){
+        addSelectedClass(element[0]);
+    }
+}
+
 function attachEvents() {
     let buttons = document.querySelectorAll("#projects button");
     let paragraphs = document.querySelectorAll("#projects p");
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener("click", () => showOnlyThisParagraph(paragraphs[i]));
     }
+    let navLinks = document.querySelectorAll(".links a");
+    setSelectedByUrl();
+    navLinks.forEach( link => {
+        link.addEventListener("click", () => addSelectedClass(link));
+    });
 }
+
