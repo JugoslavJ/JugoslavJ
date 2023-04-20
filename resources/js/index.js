78358c7676b190d5d60ce2f6d2b06b692da802b8
Jugoslav Jovicic <JugoslavJovicic@pm.me>
2023-04-20 16:16:24 +0200

    Added JS, updated styling, updated content of pages

---
+++ resources/js/index.js
@@ -0,0 +1,32 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+async function hideAllProjectDescriptions(){
+    let paragraphs = document.querySelectorAll("#projects p");
+    paragraphs.forEach( async paragraph => {
+        paragraph.style.visibility = "hidden";
+        paragraph.style.opacity = "0";
+        await delay(200);
+        paragraph.style.display = "none";
+    });
+}
+
+async function showOnlyThisParagraph(paragraph){
+    if( paragraph.style.display === "block"){
+        hideAllProjectDescriptions();
+    } else {
+        await hideAllProjectDescriptions();
+        await delay(200);
+        paragraph.style.display = "block";
+        await delay(200);
+        paragraph.style.visibility = "visible";
+        paragraph.style.opacity = "1";
+    }
+}
+
+function attachEvents() {
+    let buttons = document.querySelectorAll("#projects button");
+    let paragraphs = document.querySelectorAll("#projects p");
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener("click", () => showOnlyThisParagraph(paragraphs[i]));
+    }
+}
